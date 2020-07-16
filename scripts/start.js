b0375acf899a2d3c9a420f0c6daa75bdb6a9afa0
guofangchao <guofangchao@daling.com>
2020-07-16 20:34:16 +0800

    init

--- scripts/start.js
+++ scripts/start.js
@@ -12,7 +12,7 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require('../react_config/env');
 
 
 const fs = require('fs');
@@ -28,9 +28,9 @@ const {
   prepareUrls,
 } = require('react-dev-utils/WebpackDevServerUtils');
 const openBrowser = require('react-dev-utils/openBrowser');
-const paths = require('../config/paths');
-const configFactory = require('../config/webpack.config');
-const createDevServerConfig = require('../config/webpackDevServer.config');
+const paths = require('../react_config/paths');
+const configFactory = require('../react_config/webpack.config');
+const createDevServerConfig = require('../react_config/webpackDevServer.config');
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;

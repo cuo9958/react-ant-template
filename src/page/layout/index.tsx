54629d77b840bfdd5fa8093b47e9d32a54c525e1
guofangchao <guofangchao@daling.com>
2020-07-20 12:13:21 +0800

    增加登录界面

--- src/page/layout/index.tsx
+++ src/page/layout/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu, Avatar } from 'antd';
+import { Layout, Menu, Avatar, Button } from 'antd';
 import './index.less';
 import routes from '../../routes/config';
 
@@ -17,14 +17,15 @@ export default class extends React.Component<IProps> {
             <Layout id="main">
                 <Sider className="menus" breakpoint="lg" collapsed={false}>
                     <Menu theme="dark" defaultSelectedKeys={[this.props.active]} mode="inline" onClick={(e) => this.onMenu(e)}>
-                        {routes.map((item: any) => (
-                            <Menu.Item key={item.path}>{item.title}</Menu.Item>
-                        ))}
+                        {routes.map((item) => !item.hide && <Menu.Item key={item.path}>{item.title}</Menu.Item>)}
                     </Menu>
                 </Sider>
                 <Layout className="container">
                     <div className="header">
-                        <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>管</Avatar>
+                        <Avatar className="avatar">管</Avatar>
+                        <Button onClick={() => this.logout()} className="logout" size="small" type="primary">
+                            注销
+                        </Button>
                     </div>
                     <Content className="contents">{this.props.children}</Content>
                 </Layout>
@@ -34,5 +35,9 @@ export default class extends React.Component<IProps> {
 
     onMenu(e: any) {
         this.props.history.push(e.key);
+    }
+
+    logout() {
+        this.props.history.push('/login');
     }
 }
--- src/routes/config.ts
+++ src/routes/config.ts
@@ -1,8 +1,19 @@
 import Home from '../page/home';
 import List from '../page/list';
 import Table from '../page/table';
+import Logout from '../page/logout';
 
-export default [
+interface ICFG {
+    name: string;
+    title: string;
+    path: string;
+    page: any;
+    exact?: boolean;
+    layout?: boolean;
+    hide?: boolean;
+}
+
+const list: ICFG[] = [
     {
         name: 'home',
         title: '首页',
@@ -28,11 +39,21 @@ export default [
         layout: true,
     },
     {
+        name: 'logout',
+        title: '登录',
+        path: '/logout',
+        page: Logout,
+        exact: true,
+        layout: false,
+    },
+    {
         name: 'home2',
         title: '测试',
         path: '/test',
         page: Home,
         exact: true,
         layout: false,
+        hide: true,
     },
 ];
+export default list;
